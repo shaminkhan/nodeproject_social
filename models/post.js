425ff4ea77e6ccf2c912e087a89ac04bf389a90f
farkhandaabbas0427@gmail.com <farkhandaabbas0427@gmail.com>
2019-01-22 21:03:49 +0500

    first commit

---
+++ models/post.js
@@ -0,0 +1,21 @@
+
+var mongoose = require('mongoose');
+var Schema  = mongoose.Schema;
+
+var postSchema = new Schema({
+	description  : {type:String,trim:true},
+	experience:{type:String,trim:true},
+	image : {type:String,trim:true},
+	likes : Array,
+	comments : [{
+				 user : Object,
+				 fullname : String,
+				 username : String,
+				 comment : String
+			   }],
+	date   : {type:Date,default:Date.now()},
+	owner  : Object
+});
+
+var Post = mongoose.model('Post',postSchema);
+module.exports = Post;
